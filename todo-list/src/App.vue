66c222a3681119a261fc4745853c6b926896e308
Cyril <1075887346@qq.com>
2019-07-21 22:08:58 +0800

    add style.css

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="todoapp">
     <h1>Jquery To Do List</h1>
     <h2>Simple Todo List with adding and filter by diff status.</h2>
-    <input type="text" v-model="todo"><button>Add</button>
+    <input type="text" v-model="todo" class="todoInput">
+    <button class="addButton">Add</button>
 
     <div id="todolist">
       <div>
-        <ul v-show="list.length">
+        <ol v-show="list.length">
           <li v-for="item in list" :key="item.id">
             <div>
               <input type="checkbox" />
               <label>{{item.title}}</label>
             </div>
           </li>
-        </ul>
+        </ol>
       </div>
     </div>
 
@@ -49,32 +50,7 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+  @import './style/style.css'
 
 </style>
