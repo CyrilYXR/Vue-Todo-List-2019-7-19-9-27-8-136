329c4455e7c54885f0ebb2499cffec56d33e5998
Cyril <1075887346@qq.com>
2019-07-22 23:34:37 +0800

    update some styles

--- todo-list/src/components/header.vue
+++ todo-list/src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="head">
     <h1>Jquery To Do List</h1>
-    <h2>Simple Todo List with adding and filter by diff status.</h2>
+    <h3>Simple Todo List with adding and filter by diff status.</h3>
     <input type="text" v-model="todo" class="todoInput">
     <button class="addButton" v-on:click="addTodo(todo)">Add</button>
     
